b985f82493df829a6524077a5c323d408228e5e9
M.Nasr <mohmed.hassan28@yahoo.com>
2022-10-11 23:34:58 +0200

    roughly correct accumultion

--- src/Accumulation.cpp
+++ src/Accumulation.cpp
@@ -1,4 +1,5 @@
 #include "Accumulation.h"
+#include <bits/stdc++.h>
 
 #ifdef Accumulation_unit_test
 #include <iostream>
@@ -13,20 +14,19 @@ Accumulation::~Accumulation()
 
 int Accumulation::Calculate(std::vector<int> &Values) const {
     int sum = 0;
-    for(int i : Values) {
-        sum += i;
-    }
+    std::sort(Values.begin(), Values.end()); //ascending
+    sum = Values[4] - Values[0];
     return (sum);
 }
 
 #ifdef Accumulation_unit_test
 void Accumulation_UnitTest() {
     int samples = 5;
-    std::vector<int> Values{1,2,3,4,5};
+    std::vector<int> Values{4,2,5,1,3};
 
     Accumulation accumulation(samples);
 
-    if(accumulation.Calculate(Values) == 15) {
+    if(accumulation.Calculate(Values) == 4) {
         std::cout << "Test Pass\n";
     }
     else {
